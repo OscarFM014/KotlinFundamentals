b9b03d297b717bcd852cbc19bad9c6246bb77662
Oscar Fernandez <oscar.gohan2010@hotmail.com>
2020-08-01 15:20:57 -0500

    Basis

---
+++ A.BasicConcepts/src/2.Functions.kt
@@ -0,0 +1,20 @@
+fun main(args: Array<String>) {
+    println(3+1)
+    println(2*2)
+    println(4/2)
+
+    val a = 4
+    val b = 2
+
+    println(a.plus(b))
+    println(a.minus(b))
+    println(a.times(b))
+    println(a.div(b))
+
+    //var cen be access out of the function
+    var n = 3
+
+    println(n)
+    n=4
+    println(n)
+}
